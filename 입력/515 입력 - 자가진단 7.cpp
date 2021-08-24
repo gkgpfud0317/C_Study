a844650286b1f582ebe40c2182660a677730ffe4
하혜령 <gkgpfud1004@gmail.com>
2021-08-24 16:01:10 +0900

    515 입력 - 자가진단 7

---
+++ 입력/515 입력 - 자가진단 7.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d * %d = %d\n", a, b, a * b);
+	printf("%d / %d = %d\n", a, b, a / b);
+
+	return 0;
+
+}
