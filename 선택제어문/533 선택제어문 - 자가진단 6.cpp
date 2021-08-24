d98caae22ea0ec35a128c55dc7bb2afe5f6aa08e
하혜령 <gkgpfud1004@gmail.com>
2021-08-24 20:07:28 +0900

    533 선택제어문 - 자가진단 6

---
+++ 선택제어문/533 선택제어문 - 자가진단 6.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+	char a;
+	int b;
+	scanf("%c %d", &a, &b);
+
+	if (a == 'M') {
+		if (b >= 18)
+			printf("MAN");
+		else
+			printf("BOY");
+	}
+	else if (a == 'F') {
+		if (b >= 18)
+			printf("WOMAN");
+		else
+			printf("GIRL");
+	}
+	return 0;
+}
+
