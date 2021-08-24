ff30cdc39396f9d5bc7179856e15dedd69e8c3ae
하혜령 <gkgpfud1004@gmail.com>
2021-08-24 20:07:02 +0900

    532 선택제어문 - 자가진단 5

---
+++ 선택제어문/532 선택제어문 - 자가진단 5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	float a, b;
+	scanf("%f %f", &a, &b);
+
+	if (a >= 4.0 && b >= 4.0)
+		printf("A");
+	else if (a >= 3.0 && b >= 3.0)
+		printf("B");
+	else
+		printf("C");
+	return 0;
+}
