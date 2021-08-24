c82e872dea5ea454a398d1be9bb7323cd57362bd
하혜령 <gkgpfud1004@gmail.com>
2021-08-24 18:41:35 +0900

    518 연산자 - 자가진단 1

---
+++ 연산자/518 연산자 - 자가진단 1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int a, b, c;
+	int sum, avg;
+	scanf("%d %d %d", &a, &b, &c);
+	sum = a + b + c;
+	avg = (a + b + c) / 3;
+	printf("sum : %d\n", sum);
+	printf("avg : %d\n", avg);
+	return 0;
+}
