a24826c9d3c6c78e40e87a5f398db38d8a9b2b7e
하혜령 <gkgpfud1004@gmail.com>
2021-08-24 15:59:27 +0900

    513 입력 - 자가진단 5

---
+++ 입력/513 입력 - 자가진단 5.cpp
@@ -0,0 +1,8 @@
+#pragma warning (disable : 4996)
+#include <stdio.h>
+int main() {
+	double yd = 91.44, in = 2.54;
+	printf(" 2.1yd = %5.1lfcm\n", yd * 2.1);
+	printf("10.5in = %5.1lfcm", in * 10.5);
+	return 0;
+}
