19f655a07da89ba4a382ee27ac95061105bafeb5
하혜령 <gkgpfud1004@gmail.com>
2021-08-25 16:32:09 +0900

    147 반복제어문 3 - 형성평가 8

---
+++ 반복제어문3/147 반복제어문 3 - 형성평가 8.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int i, j, n, num = 1;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < i; j++)
+			printf("  ");
+		for (j = i; j < n; j++)
+			printf("%d ", num++);
+		printf("\n");
+	}
+	return 0;
+}
