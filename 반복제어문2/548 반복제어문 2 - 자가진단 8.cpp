a040b7d6f7dc6df040cb39e5f42f00c43d83bfff
하혜령 <gkgpfud1004@gmail.com>
2021-08-25 10:18:34 +0900

    548 반복제어문 2 - 자가진단 8

---
+++ 반복제어문2/548 반복제어문 2 - 자가진단 8.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int i, j;
+	for (i = 2; i <= 4; i++) {
+		for (j = 1; j <= 5; j++) {
+			printf("%-2d*%2d =%3d   ", i, j, i * j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
